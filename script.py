45433266d8535b948a5589b5a1e3ebe059d5e8d3
LUSIFER <70184956+lusifernoob@users.noreply.github.com>
2021-03-13 16:23:49 +0530

    Update script.py

--- script.py
+++ script.py
@@ -5,36 +5,36 @@
 
 class Script(object):
 
-    START_MSG = """<b>Hello {},
+    START_MSG = """<b>HELLO 🙂 {},
 
-I can extract audio and subtitles from video files.
+🌞I can extract audio and subtitles from video files😇.
 
 Send me any video file and extract what you want :).
 
 See <i>help</i> for more details.
 
-© @TroJanzHEX</b>"""
+© @OMG_info</b>"""
 
 
-    HELP_MSG = """<b>Hai, Follow these Steps..</b>
+    HELP_MSG = """<b>🖐️Hello, Follow these Steps🙃..</b>
 
-🌀 <i>Send me any Valid video file.</i>
+♈ <i>📩Send me any Valid video file🗂️.</i>
 
-🌀 <i>Click Download and Process button to download the file to my server</i>
+♊ <i>📥Click Download and Process button to download the file to my server</i>
 
-🌀 <i>Wait till I process the video!</i>
+🔴 <i>🕑Wait till I process the video⏳!</i>
 
-🌀 <i>Now select the stream you need to extract!</i>
+🔵 <i>Now select the stream you need to extract!</i>
 
-<b>© @TroJanzHEX</b>"""
+<b>© @OMG_info</b>"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Streams Extractor Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+💚<b>Creater :</b> @OMG_info    
 
-⭕️<b>Language :</b> <code>Python3</code>
+💜<b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+🖤<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
 """
